a05808d0302408d0daaf3bd35bf9df4e59f81f7d
Serhii Borodin <bsy.prog@gmail.com>
2024-01-09 00:16:44 +0300

    Create model House

--- house-store/houses/models.py
+++ house-store/houses/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class House(m.Model):
+    name = m.CharField(verbose_name='Название', max_length=50)
+    price = m.IntegerField(verbose_name='Цена')
+    description = m.TextField(verbose_name='Описание')
