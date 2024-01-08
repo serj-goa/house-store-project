aaf0ecda16163207df51d79ce48886884e7a3c1c
Serhii Borodin <bsy.prog@gmail.com>
2024-01-09 00:56:50 +0300

    Add verbose name

--- house-store/houses/models.py
+++ house-store/houses/models.py
@@ -5,3 +5,7 @@ class House(m.Model):
     name = m.CharField(verbose_name='Название', max_length=50)
     price = m.IntegerField(verbose_name='Цена')
     description = m.TextField(verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Дом'
+        verbose_name_plural = 'Дома'
