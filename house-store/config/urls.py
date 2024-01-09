d71f8984988a40156eba0b13af8e53fb8f662943
Serhii Borodin <bsy.prog@gmail.com>
2024-01-09 23:08:36 +0300

    Add media

--- house-store/config/urls.py
+++ house-store/config/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
@@ -22,4 +24,4 @@ from houses.views import houses_list
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', houses_list, name='houses_list'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
