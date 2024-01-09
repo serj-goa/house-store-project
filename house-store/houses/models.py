c292cf2785cf357f6993a13c2012be48ea22c0a2
Serhii Borodin <bsy.prog@gmail.com>
2024-01-09 22:48:55 +0300

    Add field photo in House model

--- house-store/houses/models.py
+++ house-store/houses/models.py
@@ -5,6 +5,7 @@ class House(m.Model):
     name = m.CharField(verbose_name='Название', max_length=50)
     price = m.IntegerField(verbose_name='Цена')
     description = m.TextField(verbose_name='Описание')
+    photo = m.ImageField(verbose_name='Фото', upload_to='houses/photos', default='', blank=True)
 
     class Meta:
         verbose_name = 'Дом'
