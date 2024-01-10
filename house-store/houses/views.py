a7fc712f70b6357b7d82669589d45e339666a878
Serhii Borodin <bsy.prog@gmail.com>
2024-01-10 20:48:16 +0300

    Update Order Form

--- house-store/houses/views.py
+++ house-store/houses/views.py
@@ -1,17 +1,26 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from houses.models import House
 from orders.forms import OrderForm
 
 
 def house_detail(request: HttpRequest, house_id: int):
-    form = OrderForm()
     house = get_object_or_404(House, id=house_id)
+    form = OrderForm(request.POST or None, initial={'house': house})
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            url = reverse('house_detail', kwargs={'house_id': house.id})
+
+            return HttpResponseRedirect(f'{url}?sent=1')
 
     context = {
         'house': house,
         'form': form,
+        'sent': request.GET.get('sent'),
     }
     template = 'houses/house_detail.html'
 
--- house-store/orders/forms.py
+++ house-store/orders/forms.py
@@ -1,9 +1,13 @@
 from django import forms as f
 
+from houses.models import House
 from orders.models import Order
 
 
 class OrderForm(f.ModelForm):
+    house = f.ModelChoiceField(queryset=House.objects.all(), widget=f.HiddenInput)
+    personal_data = f.BooleanField(label='I agree to the processing of personal data', required=True)
+
     class Meta:
         model = Order
         fields = 'house', 'name', 'phone'
